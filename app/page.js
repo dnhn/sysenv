58b638415cb060669c155ff6204c160c29b7f1e3
nhan.dev <hey@nhan.dev>
2025-07-03 00:21:03 +0700

    Footer

--- app/page.js
+++ app/page.js
@@ -113,6 +113,16 @@ export default function Home() {
           {process.env.VERCEL_GIT_PULL_REQUEST_ID && <td>{process.env.VERCEL_GIT_PULL_REQUEST_ID}</td>}
         </tr>
       </tbody>
+      <tfoot>
+        <tr>
+          <td colSpan={2} className="py-8 px-4">
+            <div className="flex gap-4 items-center justify-center text-sm font-bold">
+              <Link href="https://nhan.dev" target="_blank">nhan.dev</Link>
+              <Link href="https://github.com/dnhn/sysenv" target="_blank">GitHub</Link>
+            </div>
+          </td>
+        </tr>
+      </tfoot>
     </table>
   );
 }
