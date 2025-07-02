95537eb4762b0148241f90fb0b5e840966521bc8
nhan.dev <hey@nhan.dev>
2025-07-03 00:44:04 +0700

    Links

--- app/page.js
+++ app/page.js
@@ -34,15 +34,15 @@ export default function Home() {
         </tr>
         <tr>
           <th>VERCEL_URL</th>
-          {process.env.VERCEL_URL && <td>{process.env.VERCEL_URL}</td>}
+          {process.env.VERCEL_URL && <td><Link href={`https://${process.env.VERCEL_URL}`}>{process.env.VERCEL_URL}</Link></td>}
         </tr>
         <tr>
           <th>VERCEL_BRANCH_URL</th>
-          {process.env.VERCEL_BRANCH_URL && <td>{process.env.VERCEL_BRANCH_URL}</td>}
+          {process.env.VERCEL_BRANCH_URL && <td><Link href={`https://${process.env.VERCEL_BRANCH_URL}`}>{process.env.VERCEL_BRANCH_URL}</Link></td>}
         </tr>
         <tr>
           <th>VERCEL_PROJECT_PRODUCTION_URL</th>
-          {process.env.VERCEL_PROJECT_PRODUCTION_URL && <td>{process.env.VERCEL_PROJECT_PRODUCTION_URL}</td>}
+          {process.env.VERCEL_PROJECT_PRODUCTION_URL && <td><Link href={`https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`}>{process.env.VERCEL_PROJECT_PRODUCTION_URL}</Link></td>}
         </tr>
         <tr>
           <th>VERCEL_REGION</th>
@@ -70,15 +70,15 @@ export default function Home() {
         </tr>
         <tr>
           <th>VERCEL_GIT_PROVIDER</th>
-          {process.env.VERCEL_GIT_PROVIDER && <td>{process.env.VERCEL_GIT_PROVIDER}</td>}
+          {process.env.VERCEL_GIT_PROVIDER && <td><Link href={`https://${process.env.VERCEL_GIT_PROVIDER}.com`}>{process.env.VERCEL_GIT_PROVIDER}</Link></td>}
         </tr>
         <tr>
           <th>VERCEL_GIT_REPO_SLUG</th>
-          {process.env.VERCEL_GIT_REPO_SLUG && <td>{process.env.VERCEL_GIT_REPO_SLUG}</td>}
+          {process.env.VERCEL_GIT_REPO_SLUG && <td><Link href={`https://github.com/${process.env.VERCEL_GIT_REPO_OWNER}/${process.env.VERCEL_GIT_REPO_SLUG}`}>{process.env.VERCEL_GIT_REPO_SLUG}</Link></td>}
         </tr>
         <tr>
           <th>VERCEL_GIT_REPO_OWNER</th>
-          {process.env.VERCEL_GIT_REPO_OWNER && <td>{process.env.VERCEL_GIT_REPO_OWNER}</td>}
+          {process.env.VERCEL_GIT_REPO_OWNER && <td><Link href={`https://github.com/${process.env.VERCEL_GIT_REPO_OWNER}`}>{process.env.VERCEL_GIT_REPO_OWNER}</Link></td>}
         </tr>
         <tr>
           <th>VERCEL_GIT_REPO_ID</th>
@@ -86,11 +86,11 @@ export default function Home() {
         </tr>
         <tr>
           <th>VERCEL_GIT_COMMIT_REF</th>
-          {process.env.VERCEL_GIT_COMMIT_REF && <td>{process.env.VERCEL_GIT_COMMIT_REF}</td>}
+          {process.env.VERCEL_GIT_COMMIT_REF && <td><Link href={`https://github.com/${process.env.VERCEL_GIT_REPO_OWNER}/${process.env.VERCEL_GIT_REPO_SLUG}/tree/${process.env.VERCEL_GIT_COMMIT_REF}`}>{process.env.VERCEL_GIT_COMMIT_REF}</Link></td>}
         </tr>
         <tr>
           <th>VERCEL_GIT_COMMIT_SHA</th>
-          {process.env.VERCEL_GIT_COMMIT_SHA && <td>{process.env.VERCEL_GIT_COMMIT_SHA}</td>}
+          {process.env.VERCEL_GIT_COMMIT_SHA && <td><Link href={`https://github.com/${process.env.VERCEL_GIT_REPO_OWNER}/${process.env.VERCEL_GIT_REPO_SLUG}/commit/${process.env.VERCEL_GIT_COMMIT_SHA}`}>{process.env.VERCEL_GIT_COMMIT_SHA}</Link></td>}
         </tr>
         <tr>
           <th>VERCEL_GIT_COMMIT_MESSAGE</th>
